ae60e4d8de1775b7af46f9fa2b35e0f8c50f090c
Eodera1 <ericowino8@gmail.com>
2023-02-17 03:04:09 -0800

    Math

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19
+ * main - Entry point 
+ * Description: A program that prints exactly and that piece of art is useful" - Dora Korpar, 2015-10-19
  * Return: 0 (Success)
  */
 int main(void)
