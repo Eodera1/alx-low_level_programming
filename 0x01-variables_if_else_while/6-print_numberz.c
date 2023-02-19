083329737fb8e2bb44ef8be0406dd01bffee6f03
Eodera1 <ericowino8@gmail.com>
2023-02-19 04:04:13 -0800

    Come

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *
+ *main - single digit numbers of base 10 starting from 0
+ *Description: Entry level
+ *Return: 0 (Success)
+ */
+int main(void)
+{
+	int x;
+	for (x = 0; x < 10; x++)
+	{
+		putchar(x + '0');
+	}
+	putchar('\n');
+	return (0);
+}
