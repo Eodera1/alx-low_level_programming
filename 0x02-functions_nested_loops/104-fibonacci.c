a4d0bde71b5ffe2569eaba8cc239c47fb9639eea
Eodera1 <ericowino8@gmail.com>
2023-02-25 05:51:06 -0800

    Shoe

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - print the sum of even terms in Fibonacci sequence
+ * not exceeding 4,000,000
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+long int a, b, start, fibonacci;
+a = 1;
+b = 2;
+start = fibonacci = 0;
+
+while (fibonacci < 4000000)
+{
+	fibonacci = a + b;
+	a = b;
+	b = fibonacci;
+	if ((a % 2) == 0)
+	{
+		start += a;
+	}
+}
+printf("%ld\n", start);
+return (0);
+}
