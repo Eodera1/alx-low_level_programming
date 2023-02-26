e19f6b797ebcb0fecd7899255bf74c24551b2c81
Eodera1 <ericowino8@gmail.com>
2023-02-26 06:39:37 -0800

    Music

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiples two int values.
+ * @a: first value to check.
+ * @b: second value to check.
+ * Return: int
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
