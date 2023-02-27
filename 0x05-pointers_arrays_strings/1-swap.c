c9a139cdac34e0d633f30498f0dd9e81b6070b40
Eodera1 <ericowino8@gmail.com>
2023-02-27 11:19:54 -0800

    Water

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * Swap_int - swaps the value of two integers
+ * Swap_int - Swaps the value of two integers.
  * @a: The first integer to be swapped.
  * @b: The second integer to be swapped.
  *
@@ -9,7 +9,6 @@
  */
 
 void swap_int(int *a, int *b)
-/* the function that swaps the value of two integers */
 {
 	int temp = *a;
 	*a = *b;
