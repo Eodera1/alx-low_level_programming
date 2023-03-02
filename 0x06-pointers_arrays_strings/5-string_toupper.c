c6b72ab88d7352b75ae8e2c9f48cf734ca9a8de0
Eodera1 <ericowino8@gmail.com>
2023-03-02 01:56:53 -0800

    To upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,7 +6,7 @@
  * Return: n
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *n)
 {
 	int i;
 
@@ -14,7 +14,7 @@ char *string_toupper(char *)
 	while (n[i] != '\0')
 	{
 		if (n[i] >= 'a' && n[i] <= 'z')
-			n[i] = n(i) - 32;
+			n[i] = n[i] - 32;
 	       i++;
 	}
 	return (n);
