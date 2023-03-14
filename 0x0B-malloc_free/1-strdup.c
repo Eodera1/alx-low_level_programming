50507155b306abdcbb60f5db8dd44460270ccb63
Eodera1 <ericowino8@gmail.com>
2023-03-14 12:19:44 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -5,6 +5,7 @@
 /**
  * _strdup - duplicate to new memory space location
  * @str: char
+ *
  * * Return: 0
  */
 
@@ -14,7 +15,7 @@ char *_strdup(char *str)
 	int i, r = 0;
 
 	if (str == NULL)
-		return (NULL)
+		return (NULL);
 	i = 0;
 	while (str[i] != '\0')
 		i++;
