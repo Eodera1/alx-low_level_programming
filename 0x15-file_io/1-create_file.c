20ee3b58569f40927af574010b66d140757b9099
Erick Owino Odera <118165396+Eodera1@users.noreply.github.com>
2023-05-28 00:15:12 +0300

    Update 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,41 +1,36 @@
 #include "main.h"
-#include <string.h>
-
 /**
- * create_file - Creates a file
- * @filename:name of the file to create
- * @text_content:is a NULL terminated string to write to the file
+ * create_file - creates a file
+ * @filename:file to create
+ * @text_content:string to write on the file
  *
- * Return:1 on success, -1 on failure
+ * Return:1 on success -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd = 0;
+	int i, j, k;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
 
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC | S_IRUSR | S_IWUSR);
+	i = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (fd == -1)
-	{
+	if (i == -1)
 		return (-1);
-	}
 
-	if (text_content != NULL)
-	{
-		ssize_t len = strlen(text_content);
-		ssize_t ret = write(fd, text_content, len);
+	if (!text_content)
+		text_content = "";
 
-		if (ret == -1 || (ssize_t) ret != len)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
+	for (j = 0; text_content[j]; j++)
+		;
 
-	close(fd);
+	k = write(i, text_content, j);
+
+
+	if (k == -1)
+		return (-1);
+
+	close(i);
+
 	return (1);
 }
